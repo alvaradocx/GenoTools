45abb68ca903ca00106f73b06cdf2b74bf912678
alvaradocx <42775228+alvaradocx@users.noreply.github.com>
2022-01-11 11:27:59 -0500

    Update qc.py

--- QC/qc.py
+++ QC/qc.py
@@ -322,7 +322,7 @@ def related_prune(geno_path, out_path, related_grm_cutoff=0.125, duplicated_grm_
 
         cmds = [gcta_cmd1, gcta_cmd2, gcta_cmd3, plink_cmd1]
         for cmd in cmds:
-            shell_do(cmd, split=False)
+            shell_do(cmd)
 
     # get sample counts
     total_count = sum(1 for line in open(f'{geno_path}.fam'))
